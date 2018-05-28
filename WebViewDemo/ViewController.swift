6402e363c67fd56203821272370d51918096b2e3
Walt Boling <bolingjw@gmail.com>
2018-05-28 14:54:21 -0400

    Initial Commit

---
+++ WebViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewDemo
+//
+//  Created by Jon Boling on 5/28/18.
+//  Copyright © 2018 Walt Boling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
