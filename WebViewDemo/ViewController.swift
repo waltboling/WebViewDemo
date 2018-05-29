0e18159ba85553c7505012213a577c99de8e3351
Walt Boling <bolingjw@gmail.com>
2018-05-29 11:51:25 -0400

    adds observer to enable/disable toolbar items as they are functional.

--- WebViewDemo/ViewController.swift
+++ WebViewDemo/ViewController.swift
@@ -12,10 +12,15 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
 
+    
     var webView: WKWebView?
     
+    var refresh = UIBarButtonItem()
+    var backButton = UIBarButtonItem()
+    var forwardButton = UIBarButtonItem()
+    let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    
     override func loadView() {
-        
         webView = WKWebView()
         webView?.navigationDelegate = self
         view = webView!
@@ -28,20 +33,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let request = URLRequest(url: url!)
         webView!.load(request)
         
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView?.reload))
-        let backButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Smback") , style: .plain, target: webView, action: #selector(webView?.goBack))
-        let forwardButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Smforward"), style: .plain, target: webView, action: #selector(webView?.goForward))
-        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-
+        refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView?.reload))
+        backButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Smback") , style: .plain, target: webView, action: #selector(webView?.goBack))
+        forwardButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Smforward"), style: .plain, target: webView, action: #selector(webView?.goForward))
+    
+        backButton.isEnabled = false
+        forwardButton.isEnabled = false
+        
         toolbarItems = [refresh, spacer, backButton, spacer, forwardButton]
         navigationController?.isToolbarHidden = false
         
         webView?.allowsBackForwardNavigationGestures = true
-    
+        
+        webView?.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "loading") {
+            backButton.isEnabled = (webView?.canGoBack)!
+            forwardButton.isEnabled = (webView?.canGoForward)!
+        }
     }
 }
 
