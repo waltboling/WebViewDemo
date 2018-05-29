7de5119f1b4a650276c7062655dde43450099a3f
Walt Boling <bolingjw@gmail.com>
2018-05-29 11:00:51 -0400

    builds app with web view and basic navigation - refresh, back, and forward

--- WebViewDemo/ViewController.swift
+++ WebViewDemo/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView?
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+        webView?.navigationDelegate = self
+        view = webView!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let url = URL(string:"https://en.wikipedia.org/wiki/Main_Page")
+        let request = URLRequest(url: url!)
+        webView!.load(request)
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView?.reload))
+        let backButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Smback") , style: .plain, target: webView, action: #selector(webView?.goBack))
+        let forwardButton = UIBarButtonItem(image: #imageLiteral(resourceName: "Smforward"), style: .plain, target: webView, action: #selector(webView?.goForward))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        toolbarItems = [refresh, spacer, backButton, spacer, forwardButton]
+        navigationController?.isToolbarHidden = false
+        
+        webView?.allowsBackForwardNavigationGestures = true
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
-
-
 }
 
